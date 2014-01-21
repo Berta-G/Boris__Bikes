60286f96789dc7bd8a9ee797dbba7e02db88e890
Makis Otman <makisotman@gmail.com>
2014-01-21 15:24:01 +0000

    added alias_method for dock and wrote test for garage

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -5,4 +5,12 @@ describe Garage do
 		garage1 = Garage.new ({:capacity => 22})
 		expect(garage1.capacity).to eq(22)
 	end
+
+	it "should be able to dock and fix a bike" do 
+		bike = Bike.new
+		garage = Garage.new
+		bike.break
+		garage.dock(bike)
+		expect(garage.available_bikes).to eq([bike])
+	end
 end
